04e067f277174558b9f96e0ca427427f5c0bdaa9
Sean Keery <skibum55@hotmail.com>
2023-07-31 17:05:58 +0000

    initialize empty string

--- app.py
+++ app.py
@@ -54,7 +54,8 @@ def selecting():
     SELECT * FROM Basketball;
     ''')
     records = cur.fetchall()
-    response_string="<table>"
+    response_string=""
+    response_string+="<table>"
     for player in records:
         response_string+="<tr>"
         for info in player:
