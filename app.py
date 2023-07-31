2019439a81d0c35d4d9c1d1b8388f2abe62819b9
Sean Keery <skibum55@hotmail.com>
2023-07-31 16:36:43 +0000

    db create

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def testing():
     conn = psycopg2.connect("postgres://skuser:4X6aRfFuNCvi5dux1wCYTeFR2u5NFOQm@dpg-cj3u2sdiuie55pnqiub0-a/skflask")
     conn.close()
     return 'Database connection successful!'
+
+@app.route('/db_create')
+def testing(): 
+    conn = psycopg2.connect("postgres://skuser:4X6aRfFuNCvi5dux1wCYTeFR2u5NFOQm@dpg-cj3u2sdiuie55pnqiub0-a/skflask")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball table created successfully!'
